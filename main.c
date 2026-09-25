25890b3ac292057c59990a032258aad3926da816
deontelik-lang <deontelik@gmail.com>
2026-09-25 08:14:07 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+/*NAME; Brayan kihanga
+  REG;CT100/G/30763/26
+  */
+  #include<stdio.h>
+
+int main()
+{
+    printf("Units offered this semester\n");
+    printf("1.Introduction to programming and algorthims\n");
+    printf("2.Data structure\n");
+    printf("3.Database management system\n");
+    printf("4.Software engineering\n");
+
+}
